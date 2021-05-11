75b58245201f75537d2833553ed2ad38549844e0
BarrosAmorim <barrosamorimd@gmail.com>
2021-05-11 13:11:42 -0300

    Adicionando CSS Variables

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -5,7 +5,8 @@ const Main = () => (
     <S.Logo src="/img/logo.svg" alt="Imagem atómo" />
     <S.Title>React</S.Title>
     <S.Description>Typescript, Next JS e Styled Components</S.Description>
-    <S.Ilustration src="/img/hero-illustration.svg" alt="Imagem estudante em frente ao computador"/>
+    <S.Ilustration src="/img/hero-illustration.svg" alt="Imagem estudante em frente ao computador" />
+    <a href="">Link</a>
   </S.Wrapper>
 
 )
