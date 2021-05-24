99b0f8d3ca4155e8707dfcdb2b303ba4fb6d423e
BarrosAmorim <barrosamorimd@gmail.com>
2021-05-24 09:03:20 -0300

    Configurando SEO para as páginas com Next-SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import NextNprogress from 'nextjs-progressbar'
 import Head from 'next/head'
+import { DefaultSeo } from 'next-seo'
+import SEO from '../../next-seo.config'
 
 import GlobalStyles from '../styles/global'
 
@@ -8,13 +10,12 @@ function App ({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>My-Trips</title>
         <link rel="shortcut icon" href="img/icon-192.png" />
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
           crossOrigin="" />
-        <meta name="description" content="Projeto simples NextJS" />
       </Head>
+      <DefaultSeo {...SEO} />
       <GlobalStyles />
       <NextNprogress
         color="#f231a5"
